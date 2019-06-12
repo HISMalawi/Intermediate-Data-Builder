697d76d180ca53de23f0f6151fe5979923feb630
Matthew_Mvula <matthewmvula1@gmail.com>
2019-06-12 15:14:23 +0200

    Added Models and Relationships

---
+++ db/migrate/20190612124326_create_site_types.rb
@@ -0,0 +1,15 @@
+class CreateSiteTypes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :site_types, :primary_key => :site_type_id do |t|
+		t.string		:description
+		t.string		:site_type 
+		t.boolean		:voided, null:false, default: 0
+		t.integer		:voided_by
+		t.integer		:void_reason
+		t.datetime	:app_date_created, null:false
+		t.datetime	:app_date_updated, null:false
+      	
+      	t.timestamps
+    end
+  end
+end
