952d110037c84ea56e2b4d5e45e879676bf5ec78
Jacob Mziya <jacobmziya@gmail.com>
2019-06-12 21:35:04 +0200

    Person Address Migration foreign key.

--- db/migrate/20190612082602_create_person_addresses.rb
+++ db/migrate/20190612082602_create_person_addresses.rb
@@ -13,11 +13,4 @@ class CreatePersonAddresses < ActiveRecord::Migration[5.2]
         t.timestamps
         end
     end
-    def up
-        add_foreign_key :person_addresses, :people, column: :person_id, primary_key: :person_id
-        add_foreign_key :person_addresses, :locations, column: :district_id, primary_key: :location_id
-        add_foreign_key :person_addresses, :locations, column: :traditional_authority_id, primary_key: :location_id
-        add_foreign_key :person_addresses, :locations, column: :village_id, primary_key: :location_id
-        add_foreign_key :person_addresses, :countries, column: :country_id, primary_key: :country_id
-    end
 end
