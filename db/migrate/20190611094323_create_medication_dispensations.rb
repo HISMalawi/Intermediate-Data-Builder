a98455942c031dc6bc7495103d26bdf899811dc3
Taona Chipeta-Mpando <taochipeta@gmail.com>
2019-06-12 04:11:18 +0200

    Update 20190611094323_create_medication_dispensations.rb

--- db/migrate/20190611094323_create_medication_dispensations.rb
+++ db/migrate/20190611094323_create_medication_dispensations.rb
@@ -1,6 +1,6 @@
 class CreateMedicationDispensations < ActiveRecord::Migration[5.2]
   def change
-    create_table :medication_dispensations, :primary_key => :dispensation_id do |t|
+    create_table   :medication_dispensations, :primary_key => :dispensation_id do |t|
     	t.integer  :prescription_id
     	t.float    :quantity
     	t.integer  :voided, :limit =>2
