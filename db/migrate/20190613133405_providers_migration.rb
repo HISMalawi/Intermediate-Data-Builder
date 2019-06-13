2d0a487c133f7f1c59961fc13c2c424311921048
Matthew_Mvula <matthewmvula1@gmail.com>
2019-06-13 16:01:41 +0200

    Added Providers Relationships

--- db/migrate/20190613133405_providers_migration.rb
+++ db/migrate/20190613133405_providers_migration.rb
@@ -1,5 +1,6 @@
 class ProvidersMigration < ActiveRecord::Migration[5.2]
   def change
-  		
+  		add_foreign_key :providers, :person_names, column: :person_name_id, primary_key: :person_name_id
+       	add_foreign_key :providers, :person_types, column: :person_type_id, primary_key: :person_type_id
   end
 end
