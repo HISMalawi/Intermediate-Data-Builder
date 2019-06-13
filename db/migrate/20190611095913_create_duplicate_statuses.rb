89b0e7965e1d3cb795b8f7931f0fb4df64a4dba7
Jacob Mziya <jacobmziya@gmail.com>
2019-06-13 23:15:48 +0200

    Added not null to duplicate status metadata migration file.

--- db/migrate/20190611095913_create_duplicate_statuses.rb
+++ db/migrate/20190611095913_create_duplicate_statuses.rb
@@ -1,6 +1,6 @@
 class CreateDuplicateStatuses < ActiveRecord::Migration[5.2]
   def change
-    create_table :duplicate_statuses , :primary_key => :duplicate_status_id do |t|
+    create_table :duplicate_statuses , :primary_key => :duplicate_statuses_id do |t|
 
       t.string  :status
       t.string  :description
@@ -8,6 +8,6 @@ class CreateDuplicateStatuses < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    change_column :duplicate_statuses, :duplicate_status_id, :integer
+    change_column :duplicate_statuses, :duplicate_statuses_id, :integer
   end
 end
