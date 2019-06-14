3d0f1470ab827cbb82b711a87a4a65f1d8db8ee6
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-06-14 23:53:07 +0200

    Added levenshtein MYSQL Function for Potential Duplicate Matching

--- db/migrate/20190612195702_potential_duplicates_migration.rb
+++ db/migrate/20190612195702_potential_duplicates_migration.rb
@@ -2,7 +2,7 @@ class PotentialDuplicatesMigration < ActiveRecord::Migration[5.2]
   def change
     add_foreign_key :potential_duplicates, :people, column: :person_id_a, primary_key: :person_id
     add_foreign_key :potential_duplicates, :people, column: :person_id_b, primary_key: :person_id
-    #add_foreign_key :potential_duplicates, :duplicate_statuses, column: :duplicate_status_id, primary_key: :duplicate_status_id
+    add_foreign_key :potential_duplicates, :duplicate_statuses, column: :duplicate_status_id, primary_key: :duplicate_status_id
   end
 end
 
