24182012a071bb23156c5526ac331974ed7316d1
Matthew_Mvula <matthewmvula1@gmail.com>
2019-06-15 21:30:33 +0200

    Schema Edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 
 # Load metadata into database
 connection = ActiveRecord::Base.connection
-
+puts "...Loading Database..."
 sql = File.read('db/seed_dumps/person_types.sql') # Change path and filename as necessary
 statements = sql.split(/;$/)
 statements.pop
@@ -20,6 +20,52 @@ ActiveRecord::Base.transaction do
     connection.execute(statement)
   end
 end
+puts "...Database Loaded..."
+
+
+puts "Initialising Nations"
+
+CSV.foreach("#{Rails.root}/app/assets/data/country.csv", :headers => true) do |row|
+  Country.count
+  next if row[0].blank?
+  country = row[3]
+  if country.blank?
+    country = Country.new()
+    country.name = row[3]
+
+    country.save!
+    puts "Country Saved"
+  else
+    country = Country.new()
+    country.country_id = row[0]
+    country.name = row[3]
+
+    country.save!
+    puts "Country:  {#Country.all.name} Saved"
+  end
+
+#Other Country
+ucountry = "Other"
+if ucountry.blank?
+  ucountry = Country.new()
+  ucountry.name = "Other"
+  ucountry.save
+else
+  puts "Other Country already exists"
+end
+#Unknown Country
+ucountry = "Unknown"
+if ucountry.blank?
+  ucountry = Country.new()
+  ucountry.name = "Unknown"
+  ucountry.save
+else
+  puts "Unknown Country already exists"
+end
+
+puts "Country count : #{Country.all.count}"
+end
+
 # ending loading metadata into database
 
 # Creation of other records in Ruby below ...
