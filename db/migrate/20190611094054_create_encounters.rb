363d0374c791ce454e8c77030cd505980c1a440a
Jacob Mziya <jacobmziya@gmail.com>
2019-06-16 12:11:15 +0200

    Adding app date created and update. Till patient histories. To be continued and adding provider and creator.

--- db/migrate/20190611094054_create_encounters.rb
+++ db/migrate/20190611094054_create_encounters.rb
@@ -10,6 +10,8 @@ class CreateEncounters < ActiveRecord::Migration[5.2]
       t.bigint  :voided_by
       t.datetime  :voided_date
       t.string  :void_reason
+      t.datetime	:app_date_created, null: false
+      t.datetime	:app_date_updated, null: false
 
       t.timestamps
     end
