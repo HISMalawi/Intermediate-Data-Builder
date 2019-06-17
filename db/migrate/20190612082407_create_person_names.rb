25dc428c06a2dfd7d3c35d82d02d7291f80d9800
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-06-17 20:40:50 +0200

    Populating person address 98%

--- db/migrate/20190612082407_create_person_names.rb
+++ db/migrate/20190612082407_create_person_names.rb
@@ -4,8 +4,8 @@ class CreatePersonNames < ActiveRecord::Migration[5.2]
   def change
     create_table :person_names, primary_key: :person_name_id do |t|
       t.bigint  :person_id, null: false
-      t.string  :given_name, null: false
-      t.string  :family_name, null: false
+      t.string  :given_name
+      t.string  :family_name
       t.string  :middle_name
       t.string  :maiden_name
       t.bigint  :creator, null: false
