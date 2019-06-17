35e7bd0562f6634c56503260da58ce275e2bdf72
Jacob Mziya <jacobmziya@gmail.com>
2019-06-17 11:25:13 +0200

    Removed app date created in master definition migration

--- db/migrate/20190611095419_create_master_definitions.rb
+++ db/migrate/20190611095419_create_master_definitions.rb
@@ -9,8 +9,6 @@ class CreateMasterDefinitions < ActiveRecord::Migration[5.2]
       t.bigint  :voided_by
       t.datetime :voided_date
       t.string :void_reason
-      t.datetime  :app_date_created, null: false
-      t.datetime  :app_date_updated
 
       t.timestamps
     end
