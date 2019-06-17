ac0cec2c0ebe1c6ad3fb00d6631ce9024582c62b
Jacob Mziya <jacobmziya@gmail.com>
2019-06-17 19:26:30 +0200

    removed concept and added primary and secondary diagnosis ids in schema and migration files.

--- db/migrate/20190612193026_diagnosis_migration.rb
+++ db/migrate/20190612193026_diagnosis_migration.rb
@@ -1,7 +1,7 @@
 class DiagnosisMigration < ActiveRecord::Migration[5.2]
   def change
     add_foreign_key :diagnosis, :encounters, column: :encounter_id, primary_key: :encounter_id
-    add_foreign_key :diagnosis, :master_definitions, column: :concept_id, primary_key: :master_definition_id
-    add_foreign_key :diagnosis, :master_definitions, column: :diagnosis_type_id, primary_key: :master_definition_id
+    add_foreign_key :diagnosis, :master_definitions, column: :primary_diagnosis, primary_key: :master_definition_id
+    add_foreign_key :diagnosis, :master_definitions, column: :secondary_diagnosis, primary_key: :master_definition_id
   end
 end
