876cb6a79e4a13b45409484fe9fff62749d91108
Jacob Mziya <jacobmziya@gmail.com>
2019-06-17 15:17:12 +0200

    Included entity name in master definition to allow mapping for openmrs tables.

--- db/migrate/20190611095419_create_master_definitions.rb
+++ db/migrate/20190611095419_create_master_definitions.rb
@@ -3,8 +3,10 @@
 class CreateMasterDefinitions < ActiveRecord::Migration[5.2]
   def change
     create_table :master_definitions, primary_key: :master_definition_id do |t|
-      t.string :definition
+      t.string :definition, null: false
       t.text :description
+      t.integer :openmrs_metadata_id, null: false
+      t.string :openmrs_entity_name, null: false
       t.boolean :voided, null: false, default: 0
       t.bigint  :voided_by
       t.datetime :voided_date
