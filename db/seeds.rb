2493e03128ef500ea9059a726a30021b7b1b12ba
Jacob Mziya <jacobmziya@gmail.com>
2019-06-17 10:44:43 +0200

    Added master definition to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,65 +11,66 @@
 # Creation of other records in Ruby above ...
 
 # Load metadata into database
+puts '================ Loading SQL Metadata ====================='
+
+metadata_sql_files = %w(person_types master_definitions)
 connection = ActiveRecord::Base.connection
-puts "========================================================"
-puts "================ Loading Person Types ====================="
-puts "========================================================"
-sql = File.read('db/seed_dumps/person_types.sql') # Change path and filename as necessary
-statements = sql.split(/;$/)
-statements.pop
+(metadata_sql_files || []).each do |metadata_sql_file|
+  puts "Loading #{metadata_sql_file} metadata sql file"
+  sql = File.read("db/seed_dumps/#{metadata_sql_file}.sql")
+  statements = sql.split(/;$/)
+  statements.pop
 
-ActiveRecord::Base.transaction do
-  statements.each do |statement|
-    connection.execute(statement)
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
   end
+  puts "Loaded #{metadata_sql_file} metadata sql file successfully"
+  puts ''
 end
-puts "========================================================"
-puts "================= Person Types Loaded ====================="
-puts "========================================================"
 
+puts '================= SQL Metadata End ====================='
+puts ''
+puts ''
+puts '================= Initializing Countries ========================'
 
-puts "========================================================"
-puts "================= Initializing Nations ========================"
-puts "========================================================"
-
-CSV.foreach("#{Rails.root}/app/assets/data/country.csv", :headers => true) do |row|
+CSV.foreach("#{Rails.root}/app/assets/data/country.csv", headers: true) do |row|
   Country.count
   next if row[0].blank?
+
   country = row[3]
   if country.blank?
-    country = Country.new()
+    country = Country.new
     country.name = row[3]
     country.save!
-    puts "New Country Saved"
+    puts 'New Country Saved'
   else
-    country = Country.new()
+    country = Country.new
     country.country_id = row[0]
     country.name = row[3]
     country.save!
     puts "...#{country.name} Saved Successfully..."
   end
 
-#Other Country
-ucountry = "Other"
-if ucountry.blank?
-  ucountry = Country.new()
-  ucountry.name = "Other"
-  ucountry.save
-else
+  # Other Country
+  ucountry = 'Other'
+  if ucountry.blank?
+    ucountry = Country.new
+    ucountry.name = 'Other'
+    ucountry.save
+    end
+  # Unknown Country
+  ucountry = 'Unknown'
+  if ucountry.blank?
+    ucountry = Country.new
+    ucountry.name = 'Unknown'
+    ucountry.save
+    end
 end
-#Unknown Country
-ucountry = "Unknown"
-if ucountry.blank?
-  ucountry = Country.new()
-  ucountry.name = "Unknown"
-  ucountry.save
-else
-end
-end
-puts "========================================================"
+puts ''
 puts "======== All : #{Country.count} Countries Saved Successfully ======"
-puts "========================================================"
+puts '========================================================'
 # ending loading metadata into database
 
 # Creation of other records in Ruby below ...
