51111fbd8a6dbac86207d1834b48d5ad18c73aa7
Jacob Mziya <jacobmziya@gmail.com>
2019-06-18 19:16:13 +0200

    Added external files for patient symptoms and presenting complaints.

--- bin/ids_patient_symptoms.rb
+++ bin/ids_patient_symptoms.rb
@@ -5,13 +5,13 @@ def ids_patient_symptoms(patient_symptom)
   value_coded = get_master_def_id(patient_symptom['value_coded'])
 
   if ids_patient_history.blank?
-    puts "Creating patient history for #{patient_symptom['person_id']}"
+    puts "Creating patient symptom for #{patient_symptom['person_id']}"
     ids_patient_symptoms = Symptom.new
     ids_patient_symptoms.concept_id = concept_id
     ids_patient_symptoms.encounter_id = patient_symptom['encounter_id']
     ids_patient_symptoms.value_coded = value_coded
   else
-    puts "Updating patient history for #{patient_symptom['person_id']}"
+    puts "Updating patient symptom for #{patient_symptom['person_id']}"
     ids_patient_symptoms.concept_id = concept_id
     ids_patient_symptoms.encounter_id = patient_symptom['encounter_id']
     ids_patient_symptoms.value_coded = value_coded
