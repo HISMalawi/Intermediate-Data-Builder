1a2acf57c22b3b2e05a54b3faad9bb01aa958f84
Jacob Mziya <jacobmziya@gmail.com>
2019-06-18 15:44:04 +0200

    Added all inital methods into one method as the main method.

--- bin/ids_builder.rb
+++ bin/ids_builder.rb
@@ -655,15 +655,19 @@ SQL
   end
 end
 
-populate_people
-populate_person_names
-populate_contact_details
-populate_person_address
-update_person_type
+def methods_init
+  populate_people
+  populate_person_names
+  populate_contact_details
+  populate_person_address
+  update_person_type
 
 # initiate_de_duplication
 
-populate_encounters
-populate_diagnosis
-populate_pregnant_status
-populate_vitals
+  populate_encounters
+  populate_diagnosis
+  populate_pregnant_status
+  populate_vitals
+end
+
+methods_init
