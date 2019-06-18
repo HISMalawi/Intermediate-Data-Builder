539d8c2be4a6902673b3987651d75a453e216dae
Jacob Mziya <jacobmziya@gmail.com>
2019-06-18 20:51:21 +0200

    Uncommented back all init methods.

--- bin/ids_builder.rb
+++ bin/ids_builder.rb
@@ -628,22 +628,22 @@ def populate_presenting_complaints
 end
 
 def methods_init
-  # populate_people
-  # populate_person_names
-  # populate_contact_details
-  # populate_person_address
-  # update_person_type
+  populate_people
+  populate_person_names
+  populate_contact_details
+  populate_person_address
+  update_person_type
 
   # # initiate_de_duplication
 
-  # populate_encounters
-  # populate_diagnosis
-  # populate_pregnant_status
-  # populate_vitals
-  # populate_patient_history
-  # populate_symptoms
+  populate_encounters
+  populate_diagnosis
+  populate_pregnant_status
+  populate_vitals
+  populate_patient_history
+  populate_symptoms
   populate_side_effects
-  # populate_presenting_complaints
+  populate_presenting_complaints
 end
 
 methods_init
