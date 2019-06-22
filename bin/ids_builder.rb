16da4b77e9d85438cae3e2927067f78d4305f52c
taonam <taochipeta@gmail.com>
2019-06-22 12:23:18 +0200

    Resolved relationships in saving occupaton table

--- bin/ids_builder.rb
+++ bin/ids_builder.rb
@@ -623,7 +623,7 @@ SQL
   person_addresses.each(&method(:grouped_address))
 end
 
-3def populate_patient_history
+def populate_patient_history
   last_updated = get_last_updated('PatientHistory')
 
   patient_histories = ActiveRecord::Base.connection.select_all <<~SQL
@@ -791,7 +791,7 @@ SQL
   (occupations || []).each do |rds_occupation|
     puts "processing person_id #{rds_occupation['person_id']}"
 
-    if Occupation.find_by(person_id: rds_occupation).blank?
+    if Occupation.find_by(person_id: rds_occupation['person_id']).blank?
       person_occupation = Occupation.new
       person_occupation.person_id    = rds_occupation['person_id']
       person_occupation.occupation   = rds_occupation['value']
@@ -807,7 +807,7 @@ SQL
       puts "Successfully populated occupation with record for person #{rds_occupation['person_id']}"
     else
       person_occupation = Occupation.where(person_id: rds_occupation['person_id'])
-      person_occupation.update(eperson_id: rds_occupation['person_id'], occupation: rds_occupation['value'],
+      person_occupation.update(person_id: rds_occupation['person_id'], occupation: rds_occupation['value'],
                                person_id: rds_occupation['creator'], voided: rds_occupation['voided'],
                                voided_by: rds_occupation['voided_by'], voided_date: rds_occupation['date_voided'],
                                void_reason: rds_occupation['void_reason'], created_at: Date.today.strftime('%Y-%m-%d %H:%M:%S'),
@@ -898,14 +898,14 @@ SQL
 end
 
 def methods_init
-  # populate_people
+  #   populate_people
   #   populate_person_names
   #   populate_contact_details
   #   populate_person_address
   #   update_person_type
   #
   #   initiate_de_duplication
-  #populate_encounters
+  #   populate_encounters
   #   populate_diagnosis
   #   populate_pregnant_status
   #   populate_breastfeeding_status
@@ -919,7 +919,8 @@ def methods_init
   #   populate_family_planning
   #   populate_appointment
   #   populate_prescription
-  #populate_lab_orders
+  #  populate_lab_orders
+ #   populate_occupation
 end
 
-m
+methods_init
