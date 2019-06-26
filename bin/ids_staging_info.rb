5b6b5812be252b075f06e8fb105f000545119458
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-06-26 10:15:06 +0200

    Working on HIV Staging info 98%

--- bin/ids_staging_info.rb
+++ bin/ids_staging_info.rb
@@ -4,24 +4,36 @@ def populate_hiv_staging_info
   last_updated = get_last_updated('StagingInfo')
 
   patients = ActiveRecord::Base.connection.select_all <<SQL
-  SELECT patient_id FROM #{@rds_db}.patient WHERE updated_at >= '#{last_updated}';
+  SELECT distinct p.patient_id, p.updated_at FROM #{@rds_db}.patient p join #{@rds_db}.patient_program pp on p.patient_id = pp.patient_id
+  JOIN #{@rds_db}.patient_state ps ON pp.patient_program_id = ps.patient_program_id 
+WHERE p.updated_at >= '#{last_updated}' AND pp.program_id = 1 AND p.voided = 0 AND pp.voided = 0 order by p.updated_at;
 SQL
 
   patients.each do |patient|
     puts "processing HIV info for person_id #{patient['patient_id']}"
 
-     staging_exist = HivStagingInfo.find_by(person_id: patient['patient_id'])
+    staging_exist = HivStagingInfo.find_by(person_id: patient['patient_id'])
+    age_at_initiation = patient_age_at_initiation(patient['patient_id'])['age_at_initiation']
+    age_in_days = patient_age_at_initiation(patient['patient_id'])['age_in_days']
     if staging_exist
       #Update Staging Record
+      staging_exist.update(start_date: get_start_date(patient['patient_id']),
+                           date_enrolled: get_date_enrolled(patient['patient_id']),
+                           transfer_in: get_master_def_id(get_transfer_in(patient['patient_id'], get_date_enrolled(patient['patient_id'])), 'concept_name'),
+                           re_initiated: get_master_def_id(get_transfer_in(patient['patient_id'], get_date_enrolled(patient['patient_id'])), 'concept_name'),
+                           age_at_initiation: age_at_initiation,
+                           age_in_days_at_initiation: age_in_days,
+                           who_stage: get_who_stage(patient['patient_id']),
+                           reason_for_starting: get_reason_for_starting(patient['patient_id']))
     else
       staging_info = HivStagingInfo.new
       staging_info.person_id = patient['patient_id']
       staging_info.start_date = get_start_date(patient['patient_id'])
       staging_info.date_enrolled = get_date_enrolled(patient['patient_id'])
-      staging_info.transfer_in = get_transfer_in(patient['patient_id'], get_date_enrolled(patient['patient_id']))
-      staging_info.re_initiated = get_transfer_in(patient['patient_id'], get_date_enrolled(patient['patient_id']))
-      staging_info.age_at_initiation = patient_age_at_initiation(patient['patient_id'])['age_at_initiation'] rescue nil
-      staging_info.age_in_days_at_initiation = patient_age_at_initiation(patient['patient_id'])['age_in_days'] rescue nil
+      staging_info.transfer_in = get_master_def_id(get_transfer_in(patient['patient_id'], get_date_enrolled(patient['patient_id'])), 'concept_name')
+      staging_info.re_initiated = get_master_def_id(get_transfer_in(patient['patient_id'], get_date_enrolled(patient['patient_id'])), 'concept_name')
+      staging_info.age_at_initiation = age_at_initiation
+      staging_info.age_in_days_at_initiation = age_in_days
       staging_info.who_stage = get_who_stage(patient['patient_id'])
       staging_info.reason_for_starting = get_reason_for_starting(patient['patient_id'])
 
@@ -29,7 +41,7 @@ SQL
     end
     update_last_update('StagingInfo', patient['updated_at'])
   end
- end
+end
 
 def get_start_date(patient_id)
   start_date = ActiveRecord::Base.connection.select_one <<~SQL
@@ -59,8 +71,6 @@ SQL
 end
 
 def get_transfer_in(patient_id, date_enrolled)
-  reintiated = 'N/A'
-
   date_art_last_taken_concept = ActiveRecord::Base.connection.select_one <<~SQL
     SELECT concept_id FROM #{@rds_db}.concept_name WHERE name ='DATE ART LAST TAKEN';
   SQL
@@ -70,6 +80,10 @@ SQL
 
   no_concept = ActiveRecord::Base.connection.select_one <<SQL
   SELECT concept_id FROM #{@rds_db}.concept_name WHERE name ='NO';
+SQL
+
+  yes_concept = ActiveRecord::Base.connection.select_one <<SQL
+  SELECT concept_id FROM #{@rds_db}.concept_name WHERE name ='YES';
 SQL
 
   check_one = ActiveRecord::Base.connection.select_one <<~SQL
@@ -89,11 +103,13 @@ SQL
       AND '#{get_date_enrolled(patient_id)}' = '#{date_enrolled}' AND e.patient_id = #{patient_id} GROUP BY e.patient_id;
   SQL
   if check_one
-    reintiated = 'Re-initiated'
+    reintiated = yes_concept
   elsif check_two
-    reintiated = 'Re-initiated'
+    reintiated = yes_concept
+  else
+    reintiated = no_concept
   end
-  reintiated
+  return reintiated['concept_id']
 end
 
 def patient_age_at_initiation(patient_id)
@@ -117,23 +133,27 @@ def patient_age_at_initiation(patient_id)
               group by p.patient_id
               HAVING date_enrolled IS NOT NULL;
   SQL
-   return patient_ages_at_initiation.first
+  return patient_ages_at_initiation.first
 end
 
 def get_who_stage(patient_id)
   concept_answer_id = ActiveRecord::Base.connection.select_one <<SQL
-  SELECT value_coded, value_coded_name_id FROM #{@rds_db}.obs WHERE concept_id = 7562 AND person_id = #{patient_id};
+  SELECT value_coded, value_coded_name_id, value_text FROM #{@rds_db}.obs WHERE concept_id = 7562 AND person_id = #{patient_id};
 SQL
-begin
-  who_stage = ActiveRecord::Base.connection.select_one <<SQL
-  SELECT name FROM #{@rds_db}.concept_name WHERE concept_id = #{concept_answer_id['value_coded']} 
+  debugger if concept_answer_id['value_coded'].blank?
+  if concept_answer_id['value_coded'] && concept_answer_id['value_coded_name_id']
+    who_stage = ActiveRecord::Base.connection.select_one <<SQL
+  SELECT concept_id FROM #{@rds_db}.concept_name WHERE concept_id = #{concept_answer_id['value_coded']} 
 AND concept_name_id = #{concept_answer_id['value_coded_name_id']};
 SQL
-rescue
-  nil
-end
+  elsif concept_answer_id['value_text']
+    who_stage = ActiveRecord::Base.connection.select_one <<SQL
+  SELECT concept_id FROM #{@rds_db}.concept_name WHERE name = '#{concept_answer_id['value_text']}';
+SQL
+  end
 
-  return  who_stage['name'] rescue nil
+  return get_master_def_id(who_stage['concept_id'], 'concept_name')
+
 end
 
 def get_reason_for_starting(patient_id)
@@ -149,14 +169,10 @@ SQL
   SELECT value_coded FROM #{@rds_db}.obs WHERE person_id = #{patient_id} AND concept_id = #{reason_concept_id['concept_id']} 
 AND voided = 0 AND obs_datetime = '#{max_obs_datetime['max_date_time']}';
 SQL
-begin
-  reason_for_art_eligibility = ActiveRecord::Base.connection.select_one <<SQL
-  SELECT name FROM #{@rds_db}.concept_name WHERE concept_id = #{coded_concept_id['value_coded']} AND LENGTH(name) > 0;
+    reason_for_art_eligibility = ActiveRecord::Base.connection.select_one <<SQL
+  SELECT concept_id FROM #{@rds_db}.concept_name WHERE concept_id = #{coded_concept_id['value_coded']} AND LENGTH(name) > 0;
 SQL
-rescue
-  nil
-end
 
-return reason_for_art_eligibility['name'] rescue nil
+  return get_master_def_id(reason_for_art_eligibility['concept_id'], 'concept_name')
 
 end
