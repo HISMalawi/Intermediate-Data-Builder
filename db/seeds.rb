d6082f1fec8a2219ca7ff5b3658e96a66e3f66ec
Jacob Mziya <jacobmziya@gmail.com>
2019-06-29 20:14:45 +0200

    Added arv_drug migration and seed for arv drugs and regimens.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 # Load metadata into database
 puts '================ Loading SQL Metadata ====================='
 
-metadata_sql_files = %w(person_types master_definitions)
+metadata_sql_files = %w[person_types master_definitions drugs_and_regimens]
 connection = ActiveRecord::Base.connection
 (metadata_sql_files || []).each do |metadata_sql_file|
   puts "Loading #{metadata_sql_file} metadata sql file"
@@ -35,21 +35,23 @@ puts ''
 puts ''
 puts '================= Initializing Countries ========================'
 
-CSV.foreach("#{Rails.root}/app/assets/data/country.csv", :headers => true) do |row|
+CSV.foreach("#{Rails.root}/app/assets/data/country.csv", headers: true) do |row|
   next if row[0].blank?
+
   country = Country.new
   country.name = row[3]
   country.save!
   puts "...#{country.name} Saved Successfully..."
 end
-puts "========================================================"
+puts '========================================================'
 puts "======== All : #{Country.count} Countries Saved Successfully ======"
-puts "========================================================"
-puts "======== Initializing Districts In Malawi ======"
-puts "========================================================"
-CSV.foreach("#{Rails.root}/app/assets/data/districts_with_codes.csv", :headers => true) do |row|
+puts '========================================================'
+puts '======== Initializing Districts In Malawi ======'
+puts '========================================================'
+CSV.foreach("#{Rails.root}/app/assets/data/districts_with_codes.csv", headers: true) do |row|
   # Location.count
   next if row[0].blank?
+
   row[1] = 'Nkhata-bay' if row[1].match(/Nkhata/i)
   location = Location.new
   location.name = row[1]
@@ -58,17 +60,23 @@ CSV.foreach("#{Rails.root}/app/assets/data/districts_with_codes.csv", :headers =
   location.save!
   puts "...#{location.name} Saved Successfully..."
 end
-puts "========================================================"
+puts '========================================================'
 puts "======= All : #{Location.count} Districts Saved Successfully ======"
-puts "========================================================"
+puts '========================================================'
 file = File.open("#{Rails.root}/app/assets/data/districts.json").read
 json = JSON.parse(file)
-puts "======== Initialising TAs  and Their Respective Villages ======="
+puts '======== Initialising TAs  and Their Respective Villages ======='
 json.each do |district, traditional_authorities|
-  district_id = Location.find_by_name(district).location_id rescue nil
+  district_id = begin
+                  Location.find_by_name(district).location_id
+                rescue StandardError
+                  nil
+                end
   next if district_id.blank?
+
   traditional_authorities.each do |ta, villages|
     next if ta.blank?
+
     district_ta = Location.new
     district_ta.parent_location = district_id
     district_ta.name = ta
@@ -76,68 +84,75 @@ json.each do |district, traditional_authorities|
     ta_id = district_ta.location_id
     villages.each do |village|
       ta_village = Location.new
-      ta_village.parent_location =  ta_id
+      ta_village.parent_location = ta_id
       ta_village.name = village
       ta_village.save!
       puts "Village #{ta_village.name} of TA #{district_ta.name} of #{district} Was Saved Successfully"
     end
   end
 end
-puts "========================================================"
-puts "============= TAs and Villages Loaded Successfully ================"
-puts "========================================================"
-puts "================= Initializing Site Types  =================="
-puts "========================================================"
-CSV.foreach("#{Rails.root}/app/assets/data/site_types.csv", :headers => true) do |row|
+puts '========================================================'
+puts '============= TAs and Villages Loaded Successfully ================'
+puts '========================================================'
+puts '================= Initializing Site Types  =================='
+puts '========================================================'
+CSV.foreach("#{Rails.root}/app/assets/data/site_types.csv", headers: true) do |row|
   next if row[0].blank?
+
   site = SiteType.new
   site.site_type = row[0]
   site.description = row[1]
   site.save!
-  puts "...#{site.site_type } Saved Successfully..."
+  puts "...#{site.site_type} Saved Successfully..."
 end
-puts "========================================================"
+puts '========================================================'
 puts "======== All : #{SiteType.count} Sites Types Saved Successfully ======"
-puts "========================================================"
-puts "======= Initializing Duplicate Statuses  ======"
-puts "========================================================"
-CSV.foreach("#{Rails.root}/app/assets/data/duplicate_status.csv", :headers => true) do |row|
+puts '========================================================'
+puts '======= Initializing Duplicate Statuses  ======'
+puts '========================================================'
+CSV.foreach("#{Rails.root}/app/assets/data/duplicate_status.csv", headers: true) do |row|
   next if row[0].blank?
+
   duplicate = DuplicateStatus.new
-  duplicate.status =  row[0]
+  duplicate.status = row[0]
   duplicate.description = row[1]
   duplicate.save!
   puts "...#{duplicate.status} Saved Successfully..."
 end
-puts "========================================================"
+puts '========================================================'
 puts "======== All : #{DuplicateStatus.count} Duplicate Statuses Saved Successfully ======"
-puts "========================================================"
-puts "======= Initializing Health Facilities  ======"
-puts "========================================================"
-CSV.foreach("#{Rails.root}/app/assets/data/health_facilities.csv", :headers => true) do |row|
+puts '========================================================'
+puts '======= Initializing Health Facilities  ======'
+puts '========================================================'
+CSV.foreach("#{Rails.root}/app/assets/data/health_facilities.csv", headers: true) do |row|
   next if row[0].blank?
+
   site = Site.new
-  site_type  = ""
-  if  row[5].downcase.include?("district")
-    site_type = "District Hospital"
-  elsif row[5]=="Central"
-    site_type = "Central Hospital"
-  else
-    site_type = "Health Facility"
-  end
-  site.site_type_id = SiteType.find_by_site_type(site_type).site_type_id rescue nil
+  site_type = ''
+  site_type = if row[5].downcase.include?('district')
+                'District Hospital'
+              elsif row[5] == 'Central'
+                'Central Hospital'
+              else
+                'Health Facility'
+              end
+  site.site_type_id = begin
+                        SiteType.find_by_site_type(site_type).site_type_id
+                      rescue StandardError
+                        nil
+                      end
   site.site_name = row[3]
   site.short_name = row[1]
   site.site_description = row[6]
   site.save!
   puts "...#{site.site_name} Saved Successfully..."
 end
-puts "========================================================"
+puts '========================================================'
 puts "======== All : #{Site.count} Sites Saved Successfully ======"
-puts "========================================================"
-puts ""
-puts "======== Application Set Up Done Successfully ======"
-puts "========================================================"
+puts '========================================================'
+puts ''
+puts '======== Application Set Up Done Successfully ======'
+puts '========================================================'
 
 # ending loading metadata into database
 
