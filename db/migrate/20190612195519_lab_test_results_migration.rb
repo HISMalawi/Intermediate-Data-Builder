c1a0521a41c1ff8b98be58812ae9da5ebbb03e38
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-07-01 20:55:15 +0200

    Resolved everything

--- db/migrate/20190612195519_lab_test_results_migration.rb
+++ db/migrate/20190612195519_lab_test_results_migration.rb
@@ -1,8 +1,5 @@
 class LabTestResultsMigration < ActiveRecord::Migration[5.2]
   def change
     add_foreign_key :lab_test_results, :lab_orders, column: :lab_order_id, primary_key: :lab_order_id
-    add_foreign_key :lab_test_results, :master_definitions, column: :test_type_id, primary_key: :master_definition_id
-    add_foreign_key :lab_test_results, :master_definitions, column: :test_measure_id, primary_key: :master_definition_id
-    add_foreign_key :lab_test_results, :sites, column: :results_test_facility_id, primary_key: :site_id
   end
 end
