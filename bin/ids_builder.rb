394a3db911f83b747178eda15339facf0da0313e
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-08-05 16:36:33 +0200

    Added Logging and Handling of errors in personame

--- bin/ids_builder.rb
+++ bin/ids_builder.rb
@@ -929,34 +929,34 @@ def methods_init
     FileUtils.touch '/tmp/ids_builder.lock'
   end
 
-  # populate_people
+  populate_people
   populate_person_names
-  # populate_contact_details
-  # populate_person_address
-  # update_person_type
-  # populate_encounters
-  # populate_diagnosis
-  # populate_pregnant_status
-  # populate_breastfeeding_status
-  # populate_vitals
-  # populate_patient_history
-  # populate_symptoms
-  # populate_side_effects
-  # populate_presenting_complaints
-  # populate_tb_statuses
-  # populate_outcomes
-  # populate_family_planning
-  # populate_appointment
-  # populate_prescription
-  # populate_lab_orders
-  # populate_occupation
-  # populate_dispensation
-  # populate_relationships
-  # populate_hiv_staging_info
-  # populate_precription_has_regimen
-  # populate_lab_test_results
-  # initiate_de_duplication
-  # get_people
+  populate_contact_details
+  populate_person_address
+  update_person_type
+  populate_encounters
+  populate_diagnosis
+  populate_pregnant_status
+  populate_breastfeeding_status
+  populate_vitals
+  populate_patient_history
+  populate_symptoms
+  populate_side_effects
+  populate_presenting_complaints
+  populate_tb_statuses
+  populate_outcomes
+  populate_family_planning
+  populate_appointment
+  populate_prescription
+  populate_lab_orders
+  populate_occupation
+  populate_dispensation
+  populate_relationships
+  populate_hiv_staging_info
+  populate_precription_has_regimen
+  populate_lab_test_results
+  initiate_de_duplication
+  get_people
 
    FileUtils.rm '/tmp/ids_builder.lock' if File.file?('/tmp/ids_builder.lock')
 
