0997610b92aacd50c12fcf14f235f3fac2e16aaa
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-08-12 00:03:07 +0200

    Batched processed and exeption handled upto presenting complaints

--- bin/ids_side_effects.rb
+++ bin/ids_side_effects.rb
@@ -1,4 +1,4 @@
-def ids_side_effects(patient_side_effect)
+def ids_side_effects(patient_side_effect,failed_records)
 
   concept_id = get_master_def_id(patient_side_effect['concept_id'], 'concept_name')
   value_coded = get_master_def_id(patient_side_effect['value_coded'], 'concept_name')
@@ -14,17 +14,26 @@ def ids_side_effects(patient_side_effect)
     ids_side_effects.update(app_date_created: patient_side_effect['created_at'])
     ids_side_effects.update(app_date_updated: patient_side_effect['updated_at'])
   else
-    puts "Creating patient side effects for #{patient_side_effect['person_id']}"
-    ids_side_effects = SideEffect.new
-    ids_side_effects.concept_id = concept_id
-    ids_side_effects.encounter_id = patient_side_effect['encounter_id']
-    ids_side_effects.value_coded = value_coded
-    ids_side_effects.app_date_created = patient_side_effect['created_at']
+    begin
+      puts "Creating patient side effects for #{patient_side_effect['person_id']}"
+      ids_side_effects = SideEffect.new
+      ids_side_effects.concept_id = concept_id
+      ids_side_effects.encounter_id = patient_side_effect['encounter_id']
+      ids_side_effects.value_coded = value_coded
+      ids_side_effects.app_date_created = patient_side_effect['created_at']
 
-    if ids_side_effects.save
-      puts 'Successfully saved side effects'
-    else
-      puts 'Failed to save side effects'
+      if ids_side_effects.save
+        puts 'Successfully saved side effects'
+      else
+        puts 'Failed to save side effects'
+      end
+
+      if failed_records.include?(patient_side_effect['obs_id'].to_s)
+        remove_failed_record('PersonSideEffect', patient_side_effect['obs_id'])
+      end
+    rescue Exception => e
+      File.write('log/app_errors.log', e.message, mode: 'a')
+      log_error_records('PersonSideEffect', patient_side_effect['obs_id'].to_i)
     end
   end
 
