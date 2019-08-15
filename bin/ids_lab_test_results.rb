6fa22445132fe0d65a6e9da4f21b7aa52d5fb7fd
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-08-15 16:31:54 +0200

    Handle errors in loading LabResults

--- bin/ids_lab_test_results.rb
+++ bin/ids_lab_test_results.rb
@@ -5,7 +5,10 @@ require 'json'
 
 def populate_lab_test_results
   LabOrder.find_each do |lab_order|
-    next if lab_order['tracking_number'].blank?
+    if lab_order['tracking_number'].blank?
+       puts 'Skipping Record'
+       next
+    end
     
     last_updated = get_last_updated('LabTestResults')
 
@@ -21,33 +24,39 @@ def populate_lab_test_results
        get_lab_order_results['data']['results'].each do |measure, value|
             value.each do  | test, v|
               next if test == 'result_date'
-              lab_result = LabTestResult.find_by(lab_order_id: lab_order['lab_order_id'],
-                                                 results_test_facility: get_lab_order_details['data']['other']['receiving_lab'],
-                                                 test_type: measure,
-                                                 test_measure: test,
-                                                 sample_type: get_lab_order_details['data']['other']['sample_type'],
-                                                 test_result_date: value['result_date'])
-              if lab_result
-                lab_result.update(lab_order_id: lab_order['lab_order_id'],
-                results_test_facility: get_lab_order_details['data']['other']['receiving_lab'],
-                test_type: measure,
-                sample_type: get_lab_order_details['data']['other']['sample_type'],
-                test_measure: test,
-                test_result_date: value['result_date'],
-                result: v,
-                app_date_created: Time.now)
-              else
-                lab_result = LabTestResult.new
-                lab_result['lab_order_id'] = lab_order['lab_order_id']
-                lab_result['results_test_facility'] = get_lab_order_details['data']['other']['receiving_lab']
-                lab_result['test_type'] = measure
-                lab_result['sample_type']  = get_lab_order_details['data']['other']['sample_type']
-                lab_result['test_measure'] = test
-                lab_result['test_result_date'] = value['result_date']
-                lab_result['result'] = v
-                lab_result['app_date_created'] = Time.now
+              begin
+                  lab_result = LabTestResult.find_by(lab_order_id: lab_order['lab_order_id'],
+                                                     results_test_facility: get_lab_order_details['data']['other']['receiving_lab'],
+                                                     test_type: measure,
+                                                     test_measure: test,
+                                                     sample_type: get_lab_order_details['data']['other']['sample_type'],
+                                                     test_result_date: value['result_date'])
 
-                lab_result.save
+                if lab_result
+                  lab_result.update(lab_order_id: lab_order['lab_order_id'],
+                  results_test_facility: get_lab_order_details['data']['other']['receiving_lab'],
+                  test_type: measure,
+                  sample_type: get_lab_order_details['data']['other']['sample_type'],
+                  test_measure: test,
+                  test_result_date: value['result_date'],
+                  result: v,
+                  app_date_created: Time.now)
+                else
+                  lab_result = LabTestResult.new
+                  lab_result['lab_order_id'] = lab_order['lab_order_id']
+                  lab_result['results_test_facility'] = get_lab_order_details['data']['other']['receiving_lab']
+                  lab_result['test_type'] = measure
+                  lab_result['sample_type']  = get_lab_order_details['data']['other']['sample_type']
+                  lab_result['test_measure'] = test
+                  lab_result['test_result_date'] = value['result_date']
+                  lab_result['result'] = v
+                  lab_result['app_date_created'] = Time.now
+
+                  lab_result.save
+                end
+              rescue Exception => e
+                File.write('log/app_errors.log', e.message, mode: 'a')
+                exit
               end
             end
           end
