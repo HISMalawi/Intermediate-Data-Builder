dcc958b9eb1d3ee4144e6a2b894f2adaba97c270
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-08-17 09:12:35 +0200

    Fixed bug in presenting complaints

--- bin/ids_presenting_complaints.rb
+++ bin/ids_presenting_complaints.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 def ids_presenting_complaints(presenting_complaint, failed_records)
-  ids_presenting_complaints = PresentingComplaint.find_by(encounter_id: presenting_complaint['encounter_id'], concept_id: presenting_complaint['concept_id'])
+  ids_presenting_complaints = PresentingComplaint.find_by(encounter_id: presenting_complaint['encounter_id'],
+                                                          concept_id: presenting_complaint['concept_id'])
 
   concept_id = get_master_def_id(presenting_complaint['concept_id'], 'concept_name')
   value_coded = get_master_def_id(presenting_complaint['value_coded'], 'concept_name')
 
-  if ids_presenting_complaints
+  if ids_presenting_complaints && presenting_complaint['date_changed'] >
+                                  ids_presenting_complaints['app_date_updated']
     puts "Updating presenting complaints for #{presenting_complaint['person_id']}"
-    ids_presenting_complaints.update(concept_id: concept_id)
-    ids_presenting_complaints.update(encounter_id: presenting_complaint['encounter_id'])
-    ids_presenting_complaints.update(value_coded: value_coded)
+    ids_presenting_complaints.update(concept_id: concept_id,
+                                     encounter_id: presenting_complaint['encounter_id'],
+                                     value_coded: value_coded,
+                                     app_date_created: presenting_complaint['date_created'],
+                                     app_date_updated: presenting_complaint['date_changed'])
   else
     begin
       puts "Creating presenting complaints for #{presenting_complaint['person_id']}"
@@ -18,6 +22,8 @@ def ids_presenting_complaints(presenting_complaint, failed_records)
       ids_presenting_complaints.concept_id = concept_id
       ids_presenting_complaints.encounter_id = presenting_complaint['encounter_id']
       ids_presenting_complaints.value_coded = value_coded
+      ids_presenting_complaints.app_date_created = presenting_complaint['date_created']
+      ids_presenting_complaints.app_date_updated = presenting_complaint['date_changed']
 
       if ids_presenting_complaints.save
         puts 'Successfully saved presenting complaints'
