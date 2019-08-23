21594bcb01ab8eae6ade004ed177b3cf3def3d3a
Jacob Mziya <jacobmziya@gmail.com>
2019-08-23 13:13:32 +0200

    Fixed bugs

--- db/migrate/20190611095501_create_users.rb
+++ db/migrate/20190611095501_create_users.rb
@@ -3,9 +3,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users, primary_key: :user_id do |t|
-      t.bigint  :person_id, null: false
       t.string  :username, null: false
-      t.string  :password, null: false
+      t.string  :password_digest, null: false
       t.integer :user_role, null: false
       t.boolean :voided, null: false, default: 0
       t.bigint  :voided_by
