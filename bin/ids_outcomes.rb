caa4424aec20a36cd4f55370d4f2a47400cea14f
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-09-07 18:29:51 +0200

    Added a few more fixes to the script

--- bin/ids_outcomes.rb
+++ bin/ids_outcomes.rb
@@ -1,48 +1,43 @@
 def ids_outcomes(rds_outcomes)
-      if Outcome.find_by(person_id: rds_outcomes['patient_id'],
+
+  outcome_reason = begin
+                    MasterDefinition.find_by(openmrs_metadata_id: rds_outcomes['concept_id']).master_definition_id
+                   rescue StandardError
+                    nil
+                   end
+   outcome_source = begin
+                    MasterDefinition.find_by(openmrs_metadata_id: rds_outcomes['program_id']).master_definition_id
+                   rescue StandardError
+                    nil
+                   end
+    if Outcome.find_by(person_id: rds_outcomes['patient_id'],
                          concept_id: rds_outcomes['concept_id']).blank?
         #begin
           outcome = Outcome.new
-          outcome.person_id = rds_outcomes['patient_id']
-          outcome.concept_id = rds_outcomes['concept_id']
-          outcome.outcome_reason = begin
-            MasterDefinition.find_by(openmrs_metadata_id: rds_outcomes['concept_id']).master_definition_id
-                                   rescue StandardError
-                                     nil
-          end
-          outcome.outcome_source = begin
-            MasterDefinition.find_by(openmrs_metadata_id: rds_outcomes['program_id']).master_definition_id
-                                   rescue StandardError
-                                     nil
-          end
-          outcome.voided = rds_outcomes['retired']
-          outcome.voided_by = rds_outcomes['changed_by']
-          outcome.voided_date = rds_outcomes['date_voided'] rescue nil
-          outcome.void_reason = rds_outcomes['void_reason'] rescue nil
-          outcome.app_date_created = rds_outcomes['date_created']
-          outcome.app_date_updated = rds_outcomes['date_changed']
+          outcome.person_id         = rds_outcomes['patient_id']
+          outcome.concept_id        = rds_outcomes['concept_id']
+          outcome.outcome_reason    = outcome_reason
+          outcome.outcome_source    = outcome_source
+          outcome.voided            = rds_outcomes['retired']
+          outcome.voided_by         = rds_outcomes['changed_by']
+          outcome.voided_date       = rds_outcomes['date_voided'] rescue nil
+          outcome.void_reason       = rds_outcomes['void_reason'] rescue nil
+          outcome.app_date_created  = rds_outcomes['date_created']
+          outcome.app_date_updated  = rds_outcomes['date_changed']
           outcome.save
 
-        #     remove_failed_record('outcomes', outcome['patient_program_id'].to_i)
+        #   remove_failed_record('outcomes', outcome['patient_program_id'].to_i)
         # rescue Exception => e
         #   log_error_records('outcomes', outcome['patient_program_id'].to_i, e)
         # end
     else
       outcome = Outcome.find_by(person_id: rds_outcomes['patient_id'],
                                 concept_id: rds_outcomes['concept_id'])
-      outcome.update(person_id: rds_outcomes['patient_id'])
-      outcome.update(concept_id: rds_outcomes['concept_id'])
-      begin
-        outcome.update(outcome_reason: MasterDefinition.find_by(openmrs_metadata_id: rds_outcomes['concept_id']).master_definition_id)
-      rescue StandardError
-        nil
-      end
-      begin
-        outcome.update(outcome_source: MasterDefinition.find_by(openmrs_metadata_id: rds_outcomes['program_id']).master_definition_id)
-      rescue StandardError
-        nil
-      end
-      outcome.update(voided: (rds_outcomes['voided'] || 0),
+      outcome.update(person_id: rds_outcomes['patient_id'],
+                    concept_id: rds_outcomes['concept_id'],
+                    outcome_reason: outcome_reason,
+                    outcome_source: outcome_source,
+                    voided: (rds_outcomes['voided'] || 0),
                      voided_by: rds_outcomes['voided_by'],
                      voided_date: rds_outcomes['date_voided'],
                      void_reason: rds_outcomes['void_reason'],
