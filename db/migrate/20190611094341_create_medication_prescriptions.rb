c93358ccb8fafb092380ce8de334c107aee2ecb0
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-09-15 19:54:38 +0200

    More bug fixes

--- db/migrate/20190611094341_create_medication_prescriptions.rb
+++ db/migrate/20190611094341_create_medication_prescriptions.rb
@@ -12,6 +12,7 @@ class CreateMedicationPrescriptions < ActiveRecord::Migration[5.2]
       t.bigint :voided_by
       t.datetime :voided_date
       t.string :void_reason
+      t.bigint :creator
       t.datetime  :app_date_created, null: false
       t.datetime  :app_date_updated
 
