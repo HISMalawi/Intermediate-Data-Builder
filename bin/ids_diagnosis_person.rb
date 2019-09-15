08f0161752092a0127081761797924aa382e1c4c
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-09-15 12:34:33 +0200

    Fixed bugs in Some queries in history etc

--- bin/ids_diagnosis_person.rb
+++ bin/ids_diagnosis_person.rb
@@ -9,24 +9,12 @@ def ids_diagnosis_person(diag)
   value_coded = get_master_def_id(diag['value_coded'], 'concept_name')
   
   diag = handle_commons(diag)
-  diagnosis = Diagnosis.find_by_diagnosis_id(diag['diagnosis_id'])
+  diagnosis = Diagnosis.find_by_diagnosis_id(diag['obs_id'])
 
   primary_diag  = (diag['concept_id'] == primary_diagnosis ? value_coded : '')
   secondary_diag = (diag['concept_id'] == secondary_diagnosis ? value_coded : '')
 
-  if diagnosis && check_latest_record(diag, diagnosis)    
-    diagnosis.update(
-    encounter_id: diag['encounter_id'].to_i,
-    primary_diagnosis: primary_diag,
-    secondary_diagnosis: secondary_diag,
-    voided: diag['voided'].to_i, 
-    voided_by: diag['voided_by'].to_i,
-    voided_date: diag['date_voided'],
-    void_reason: diag['void_reason'],
-    creator: diag['creator'].to_i, 
-    app_date_created: diag['date_created'],
-    app_date_updated: diag['date_changed'])   
-  else
+  if diagnosis.blank?
     begin
      Diagnosis.create(
         diagnosis_id: diag['obs_id'].to_i,
@@ -44,6 +32,18 @@ def ids_diagnosis_person(diag)
       remove_failed_record('diagnosis', diag['obs_id'].to_i)
     rescue Exception => e
       log_error_records('diagnosis', diag['obs_id'].to_i, e)
-    end
+    end     
+  elsif check_latest_record(diag, diagnosis)
+    diagnosis.update(
+    encounter_id: diag['encounter_id'].to_i,
+    primary_diagnosis: primary_diag,
+    secondary_diagnosis: secondary_diag,
+    voided: diag['voided'].to_i, 
+    voided_by: diag['voided_by'].to_i,
+    voided_date: diag['date_voided'],
+    void_reason: diag['void_reason'],
+    creator: diag['creator'].to_i, 
+    app_date_created: diag['date_created'],
+    app_date_updated: diag['date_changed'])  
   end
 end
