c19cf5fdc0dbb35891d03a0cf9c11aeb23eff67f
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-09-16 10:49:32 +0200

    Added de_duplicator person_id to be unique

--- db/migrate/20190612133826_create_de_duplicators.rb
+++ db/migrate/20190612133826_create_de_duplicators.rb
@@ -1,7 +1,7 @@
 class CreateDeDuplicators < ActiveRecord::Migration[5.2]
   def change
     create_table :de_duplicators, :primary_key => :de_duplicator_id do |t|
-      t.bigint   :person_id, null: false
+      t.bigint   :person_id, null: false, unique: true
       t.string   :person_de_duplicator, null: false
 
       t.timestamps
