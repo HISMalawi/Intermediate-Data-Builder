2f9ec0e58ee5f2df90bf3497aefb75fb3656f5d6
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-09-19 12:55:10 +0200

    Added migration for Adding Openmrs Site_code to Sites table

--- db/migrate/20190612082003_create_sites.rb
+++ db/migrate/20190612082003_create_sites.rb
@@ -5,7 +5,6 @@ class CreateSites < ActiveRecord::Migration[5.2]
     create_table :sites, primary_key: :site_id do |t|
       t.string  :site_name
       t.string  :short_name
-      t.integer :site_code
       t.string  :site_description
       t.integer  :site_type_id
       t.boolean  :voided, null: false, default: 0
