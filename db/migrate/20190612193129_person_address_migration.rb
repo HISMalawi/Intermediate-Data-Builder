450599d56db86f5671ad640dfe97ac8360e3a277
Fredrick Chiyenda <fchiyenda@gmail.com>
2020-02-10 16:49:16 +0200

    Fixed some bugs

--- db/migrate/20190612193129_person_address_migration.rb
+++ db/migrate/20190612193129_person_address_migration.rb
@@ -3,12 +3,12 @@
 class PersonAddressMigration < ActiveRecord::Migration[5.2]
   def change
     add_foreign_key :person_addresses, :people, column: :person_id, primary_key: :person_id
-    add_foreign_key :person_addresses, :locations, column: :home_district_id, primary_key: :location_id
-    add_foreign_key :person_addresses, :locations, column: :home_traditional_authority_id, primary_key: :location_id
-    add_foreign_key :person_addresses, :locations, column: :home_village_id, primary_key: :location_id
-    add_foreign_key :person_addresses, :locations, column: :current_district_id, primary_key: :location_id
-    add_foreign_key :person_addresses, :locations, column: :current_traditional_authority_id, primary_key: :location_id
-    add_foreign_key :person_addresses, :locations, column: :current_village_id, primary_key: :location_id
-    add_foreign_key :person_addresses, :countries, column: :country_id, primary_key: :country_id
+    #add_foreign_key :person_addresses, :locations, column: :home_district_id, primary_key: :location_id
+    #add_foreign_key :person_addresses, :locations, column: :home_traditional_authority_id, primary_key: :location_id
+    #add_foreign_key :person_addresses, :locations, column: :home_village_id, primary_key: :location_id
+    #add_foreign_key :person_addresses, :locations, column: :current_district_id, primary_key: :location_id
+    #add_foreign_key :person_addresses, :locations, column: :current_traditional_authority_id, primary_key: :location_id
+    #add_foreign_key :person_addresses, :locations, column: :current_village_id, primary_key: :location_id
+    #add_foreign_key :person_addresses, :countries, column: :country_id, primary_key: :country_id
   end
 end
