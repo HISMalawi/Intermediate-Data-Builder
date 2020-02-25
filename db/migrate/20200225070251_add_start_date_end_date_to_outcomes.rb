f690a2b1d178e9c978da83c7b15968e74a645e6e
Fredrick Chiyenda <fchiyenda@gmail.com>
2020-02-25 18:31:29 +0200

    Added start_date and end date to outcomes

--- db/migrate/20200225070251_add_start_date_end_date_to_outcomes.rb
+++ db/migrate/20200225070251_add_start_date_end_date_to_outcomes.rb
@@ -1,6 +1,6 @@
 class AddStartDateEndDateToOutcomes < ActiveRecord::Migration[5.2]
   def change
-  	add_column :outcomes, :start_date, :datetime
-  	add_column :outcomes, :end_date, :datetime
+  	add_column :outcomes, :start_date, :date
+  	add_column :outcomes, :end_date, :date
   end
 end
