da7cbd964e5a35b553513ce72ee2a31641bc3802
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-06-18 09:38:07 +0200

    Added errors logging and skipping when errors are encountered when loading soundexes

--- bin/populate_soundex.rb
+++ bin/populate_soundex.rb
@@ -7,9 +7,13 @@ def populate_soundex(last_updated,current_datetime,count)
 		#print "Processing #{i+1} / #{count} \r"
 		next unless DeDuplicator.exists?(person_id: person.person_id) #skip if corresponding duplicator does not exist.
 		next if person.given_name.blank? || person.family_name.blank?
-
-		Soundex.find_or_create_by(person_id: person.person_id,first_name: person.given_name.soundex,
+    begin
+			Soundex.find_or_create_by(person_id: person.person_id,first_name: person.given_name.soundex,
 							last_name:  person.family_name.soundex)
+		rescue => e
+			File.write("#{Rails.root}/log/soundex_errors.log", e, mode: 'a')
+			next
+		end
 	end
     update_last_update('Soundex', last_update) #Update the updated timestamp from the source 
 end
