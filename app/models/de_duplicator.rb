19b8f2462b8730a0c49f5ff644e13bc71ead1c71
fchiyenda <fchiyenda@gmail.com>
2023-01-12 12:53:57 +0200

    Resolved deduplicator conflict

--- app/models/de_duplicator.rb
+++ app/models/de_duplicator.rb
@@ -1,6 +1,3 @@
 class DeDuplicator < ApplicationRecord
-<<<<<<< HEAD
-=======
 	has_one :soundex, foreign_key: :person_id, primary_key: :person_id
->>>>>>> a21f366acf67dc3014e7e0d13e087d72277d2e8d
 end
